7658470c84c23820fe6d30eb851b689f8818ad6f
AndresGarciaEscalante <andres_ge@hotmail.es>
2021-11-03 22:17:16 -0600

    feat: Subscribe to the depth map topic and display it

--- dev_ws/src/tl_perception/tl_perception/depth_estimation.py
+++ dev_ws/src/tl_perception/tl_perception/depth_estimation.py
@@ -17,21 +17,30 @@ class MinimalSubscriber(Node):
         super().__init__('depth_estimation')
         self.bridge = CvBridge()
         # Subscribers
+        ## Prediction Image subscription
         #self.subscription = self.create_subscription(
         #    Image,
         #    '/detections/image',
         #    self.listener_callback,
         #    10)
         #self.subscription  # prevent unused variable warning
-        self.prediction_sub = self.create_subscription(
-            TLPredictions,
-            'detection/model/predictions',
-            self.listener_callback,
+        ## Depth Map Image subscription
+        self.depth_sub = self.create_subscription(
+            Image,
+            '/zed/zed_node/depth/depth_registered',
+            self.depth_estimation_callback,
             10)
-        self.prediction_sub
+        self.depth_sub
+        ## Predictions BBs and classes subscription
+        #self.prediction_sub = self.create_subscription(
+        #    TLPredictions,
+        #    'detection/model/predictions',
+        #    self.listener_callback,
+        #    10)
+        #self.prediction_sub
 
-    def listener_callback(self, msg):
-        print(msg)
+    #def listener_callback(self, msg):
+        #print(msg)
         #img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         #print(img)
         #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -40,6 +49,11 @@ class MinimalSubscriber(Node):
         #cv2.imshow("Traffic Light Detections", cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
         #cv2.waitKey(3)
 
+    def depth_estimation_callback(self, msg):
+        # depth map dimension is 360,640
+        img = self.bridge.imgmsg_to_cv2(msg, "passthrough")
+        cv2.imshow("Depth Map Estimation", img)
+        cv2.waitKey(3)
 
 def main(args=None):
     # ROS
