e40b8ad9006b4b42f0108d1fedfd9f33917a4438
AndresGarciaEscalante <andres_ge@hotmail.es>
2021-11-12 12:16:26 -0600

    test: estimate the distance of the centroid of the depth map and display it

--- dev_ws/src/tl_perception/tl_perception/depth_estimation.py
+++ dev_ws/src/tl_perception/tl_perception/depth_estimation.py
@@ -26,27 +26,40 @@ class MinimalSubscriber(Node):
 
     def listener_callback(self, prediction_msg, depth_msg):
         #Predicton Message
-        header = prediction_msg.header 
+        header = prediction_msg.header
         classes = prediction_msg.classes
         bbs = prediction_msg.boundingboxes
        # print(header)
-        print(classes)
-        print(bbs)
-
+       # print(classes)
+       # print(bbs)
+        #Create a empty distance array
+        depth_estimation = []
         #Depth map dimension is 360,640
         depth_img = self.bridge.imgmsg_to_cv2(depth_msg, "passthrough")
+        depth_array = np.array(depth_img, dtype=np.float32)
         #Copy the depth map image and convert it to rgb to verify that the detections are well detected
         copy = depth_img.copy()
         copy = cv2.cvtColor(copy, cv2.COLOR_GRAY2RGB)
-        #Draw a bounding box to the traffic light detections
-        for i,tlclass in enumerate(classes): # 1 0 3 2
-          cv2.rectangle(copy, (bbs[i*4+1],bbs[i*4]), (bbs[i*4+3], bbs[i*4+2]), (0,255,0), 2)
-          ## Calculate the centroid of the TL
-          x = int((bbs[i*4+3] - bbs[i*4+1])/2 + bbs[i*4+1])
-          y = int((bbs[i*4+2] - bbs[i*4])/2 + bbs[i*4])
-          cv2.circle(copy, (x,y), 5, (0,0,255), -1)
+        # Display the depth image with a point in the center
+        print("The distance of the center is :", depth_array[180,320])
+        cv2.circle(copy, (320,180), 5, (0,0,255), -1)
         cv2.imshow("Depth Map Estimation", copy)
         cv2.waitKey(3)
+
+        #Draw a bounding box to the traffic light detections
+        ###for i,tlclass in enumerate(classes): # 1 0 3 2
+        ###  cv2.rectangle(copy, (bbs[i*4+1],bbs[i*4]), (bbs[i*4+3], bbs[i*4+2]), (0,255,0), 2)
+          ## Calculate the centroid of the TL
+        ###  x = int((bbs[i*4+3] - bbs[i*4+1])/2 + bbs[i*4+1])
+        ###  y = int((bbs[i*4+2] - bbs[i*4])/2 + bbs[i*4])
+        ###  cv2.circle(copy, (x,y), 5, (0,0,255), -1)
+        ###  print(depth_array[x,y])
+          #depth_estimation.append(depth_array[x,y])
+        #print(depth_estimation)
+        #print(depth_array.shape)
+        #print("---------------------------------")
+        ##cv2.imshow("Depth Map Estimation", copy)
+        ##cv2.waitKey(3)
 
 def main(args=None):
     # ROS
@@ -64,4 +77,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
